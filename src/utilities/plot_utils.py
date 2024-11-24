c4e79ad1beaac2b9ec2f635fd40d257e3fc6d415
Ross Mcphee <rossmcphee82@gmail.com>
2024-11-24 00:07:17 +0000

    Basic isothermal processing plotter implemented

--- src/utilities/plot_utils.py
+++ src/utilities/plot_utils.py
@@ -7,3 +7,9 @@ def plot_pv_diagram(volumes, pressures, title="PV Diagram"):
     plt.ylabel("Pressure (Pa)")
     plt.grid(True)
     plt.show()
+
+def plot_isothermal_process(initial_volume, final_volume, pressure, n_moles, temperature):
+    R = 8.314
+    volumes = [v for v in range(initial_volume, final_volume + 1)]
+    pressures = [(n_moles * R * temperature) / v for v in volumes]
+    plot_pv_diagram(volumes, pressures, title="Isothermal Process")
